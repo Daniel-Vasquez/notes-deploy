cbd0c7f6719f0ddb504774ce3149d3108c3e42d9
Daniel Vasquez <daniel.vasquez.git@gmail.com>
2023-09-04 17:44:49 -0600

    Remove notes.json

--- server/models/noteModel.js
+++ server/models/noteModel.js
@@ -1,6 +1,18 @@
 import { randomUUID } from "node:crypto"
-import { readJSON } from "../utils.js";
-const notes = readJSON('./notes.json')
+// import { readJSON } from "../utils.js";
+// const notes = readJSON('./notes.json')
+const notes = [
+  {
+    "id": "1693845439847",
+    "title": "Evolución constante",
+    "content": "La tecnología avanza rápidamente y lo que es innovador hoy puede volverse obsoleto en poco tiempo."
+  },
+  {
+    "id": "1693843949847",
+    "title": "Impacto en la sociedad",
+    "content": "La tecnología ha transformado la forma en que vivimos, trabajamos y nos relacionamos, generando tanto oportunidades como desafíos."
+  }
+]
 
 export class NoteModel {
   static async getNotes() {
