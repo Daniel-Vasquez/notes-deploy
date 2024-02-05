a33862f4f48c0798a93bda6ba61443689b535775
Daniel Vasquez <daniel.vasquez.git@gmail.com>
2024-02-04 19:04:36 -0600

    Adding phrases

--- server/models/noteModel.js
+++ server/models/noteModel.js
@@ -2,7 +2,60 @@ import { randomUUID } from "node:crypto"
 // import { getDateTime } from "../utils.js";
 // import { loadData } from "../utils.js";
 
-const NOTES = []
+const NOTES = [
+  {
+    "id": "1693845439877",
+    "content": "La tecnología avanza rápidamente y lo que es innovador hoy puede volverse obsoleto en poco tiempo."
+  },
+  {
+    "id": "1693845439847",
+    "content": "La tecnología avanza rápidamente y lo que es innovador hoy puede volverse obsoleto en poco tiempo."
+  },
+  {
+    "id": "1693843949847",
+    "content": "Hay una fuerza motriz más poderosa que el vapor, la electricidad y la energía atómica: la voluntad – Albert Einstein."
+  },
+  {
+    "id": "1693843954012",
+    "content": "La única manera de hacer un gran trabajo es amar lo que haces."
+  },
+  {
+    "id": "1693843954013",
+    "content": "La vida es lo que pasa mientras estás ocupado haciendo otros planes."
+  },
+  {
+    "id": "1693843954014",
+    "content": "No hay más grande error que no hacer nada porque solo puedes hacer un poco."
+  },
+  {
+    "id": "1693843954015",
+    "content": "La única constante en la vida es el cambio."
+  },
+  {
+    "id": "1693843954016",
+    "content": "La educación es el arma más poderosa que puedes usar para cambiar el mundo."
+  },
+  {
+    "id": "1693843954017",
+    "content": "La imaginación es más importante que el conocimiento."
+  },
+  {
+    "id": "1693843954018",
+    "content": "No puedes esperar resultados diferentes si sigues haciendo lo mismo."
+  },
+  {
+    "id": "1693843954019",
+    "content": "La verdad te hará libre, pero primero te hará enfadar."
+  },
+  {
+    "id": "1693843954020",
+    "content": "La vida es 10% lo que nos sucede y 90% cómo reaccionamos ante ello."
+  },
+  {
+    "id": "1693843954021",
+    "content": "No dejes que lo que no puedes hacer interfiera con lo que puedes hacer."
+  }
+];
 
 export class NoteModel {
   static async getNotes() {
@@ -20,7 +73,7 @@ export class NoteModel {
       ...input,
     }
 
-    NOTES.unshift(newNote);
+    NOTES.push(newNote);
 
     return newNote;
   }
